60ba7273e471fe45e87ec269b134ce3df7c81bbf
Senem <64867366+senemyalin@users.noreply.github.com>
2023-03-13 23:45:26 +0300

    Added Credit Card Screen
    Added Credit Card Screen

--- app/src/main/java/com/juniorkekod/disneyplusclonekekod/presentation/register/RegisterCreditCardFragment.kt
+++ app/src/main/java/com/juniorkekod/disneyplusclonekekod/presentation/register/RegisterCreditCardFragment.kt
@@ -1,60 +1,41 @@
 package com.juniorkekod.disneyplusclonekekod.presentation.register
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import com.juniorkekod.disneyplusclonekekod.R
+import com.juniorkekod.disneyplusclonekekod.databinding.FragmentRegisterCreditCardBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [RegisterCreditCardFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class RegisterCreditCardFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentRegisterCreditCardBinding
+    private val args: RegisterCreditCardFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register_credit_card, container, false)
+        binding = FragmentRegisterCreditCardBinding.inflate(layoutInflater)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RegisterCreditCardFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RegisterCreditCardFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setView()
     }
+
+    private fun setView() {
+        if (args.type == "ANNUAL") {
+            binding.amount.text = getString(R.string.key_campaign_annual)
+            binding.type.text = getString(R.string.key_register_year)
+        } else {
+            binding.amount.text = getString(R.string.key_campaign_monthly)
+            binding.type.text = getString(R.string.key_register_month)
+        }
+    }
+
 }
