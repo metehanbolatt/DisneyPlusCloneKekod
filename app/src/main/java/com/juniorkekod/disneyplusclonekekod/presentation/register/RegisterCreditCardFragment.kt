5ffdecfa883a9671186d35361ffb6a8701d36a8a
Senem <64867366+senemyalin@users.noreply.github.com>
2023-03-20 21:34:31 +0300

    Added dimens to xml files
    Added dimens to xml files

--- app/src/main/java/com/juniorkekod/disneyplusclonekekod/presentation/register/RegisterCreditCardFragment.kt
+++ app/src/main/java/com/juniorkekod/disneyplusclonekekod/presentation/register/RegisterCreditCardFragment.kt
@@ -1,9 +1,13 @@
 package com.juniorkekod.disneyplusclonekekod.presentation.register
 
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.style.ForegroundColorSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.juniorkekod.disneyplusclonekekod.R
@@ -30,12 +34,58 @@ class RegisterCreditCardFragment : Fragment() {
 
     private fun setView() {
         if (args.type == PurchaseTypes.ANNUAL) {
-            binding.amount.text = getString(R.string.key_campaign_annual)
-            binding.type.text = getString(R.string.key_register_year)
+            //ANNUAL TEXT
+            val annualPriceText = getString(R.string.key_campaign_annual)
+            val yearText = getString(R.string.key_register_year)
+            val combinedYearlyText = getString(
+                R.string.key_purchase_type,
+                annualPriceText,
+                yearText
+            )
+
+            setYearAndMonthColor(
+                combinedYearlyText,
+                yearText,
+                RegisterCreditCardFragment.PurchaseTypes.ANNUAL
+            )
         } else {
-            binding.amount.text = getString(R.string.key_campaign_monthly)
-            binding.type.text = getString(R.string.key_register_month)
+            //MONTHLY TEXT
+            val monthlyPriceText = getString(R.string.key_campaign_monthly)
+            val monthText = getString(R.string.key_register_month)
+            val combinedMonthlyText = getString(
+                R.string.key_purchase_type,
+                monthlyPriceText,
+                monthText
+            )
+
+            setYearAndMonthColor(
+                combinedMonthlyText,
+                monthText,
+                RegisterCreditCardFragment.PurchaseTypes.MONTHLY
+            )
         }
+    }
+
+    private fun setYearAndMonthColor(
+        combinedText: String,
+        coloredText: String,
+        type: RegisterCreditCardFragment.PurchaseTypes
+    ) {
+        val colorOfText = ContextCompat.getColor(requireContext(), R.color.white_45_opacity)
+
+        val startingIndexOfText = combinedText.indexOf(coloredText)
+        val endingIndexOfText = combinedText.length
+
+
+        binding.paymentLayout.text = SpannableStringBuilder(combinedText).apply {
+            setSpan(
+                ForegroundColorSpan(colorOfText),
+                startingIndexOfText,
+                endingIndexOfText,
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+            )
+        }
+
     }
 
     enum class PurchaseTypes {
